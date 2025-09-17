f1ba9822f3476cd523bc90dca4426cae53e2feda
DartM555 <williammejia1720@gmail.com>
2025-09-17 08:56:59 -0400

    conditionals continues

---
+++ Lesson1-Conditionals/conditionals.py
@@ -0,0 +1,45 @@
+# ========================================
+# ACCELERATED PYTHON UNIT 2 - LESSON 1
+# Conditionals: JavaScript to Python
+# ========================================
+
+# ========================================
+# SECTION 1: QUICK TRANSLATION CHALLENGE
+# ========================================
+print("Grade Classification")
+# score = 87
+# if score >= 90:
+#     print("A grade!")
+# elif score >= 80:
+#     print("B grade!")
+# else:
+#     print("Below B")
+
+
+# ========================================
+# SECTION 2: AGE CATEGORY CLASSIFIER
+# ========================================
+age = input("Enter your age: ")
+
+if 0 <= age <=12:
+    print("You are a child")
+elif 13 <= age <= 19:
+    print("you are a teenager")
+elif 20 <= age <= 64:
+    print("you are an adult")
+else: age >= 65 
+print("you are a senior citizen")
+
+# ========================================
+# SECTION 3: STUDENT STATUS CHECKER
+# ========================================
+
+
+# ========================================
+# SECTION 4: GRADE VALIDATOR CHALLENGE
+# ========================================
+
+
+# ========================================
+# SECTION 5: WEATHER DECISION SYSTEM
+# ==========
