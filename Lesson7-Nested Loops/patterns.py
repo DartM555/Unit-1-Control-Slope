abbaa4495ff161e67ac177a848ff856340599ad8
DartM555 <williammejia1720@gmail.com>
2025-10-10 08:44:09 -0400

    Gemici's example

--- Lesson7-Nested Loops/patterns.py
+++ Lesson7-Nested Loops/patterns.py
@@ -1,7 +1,8 @@
 # Pyramid Pattern
-# Row 0: 4 spaces, 1 star => 0 spaces, 9 stars
+
 
 rows = 5
+
 spaces = rows - 1
 for row in range(1, rows + 1):
    for space in range(spaces):
@@ -12,8 +13,18 @@ for row in range(1, rows + 1):
            print("*", end="")
            print()
         
+        
+# Mr Gemici's Example
 
+rows = 5
+# create an outer loop for the rows
 
-
-
-9
+for i in range(rows):
+    #Step 2: print the spaces -- rows - i - 1
+    for j in range(rows-i-1):
+        print(" ", end ="")
+        # Step 3: print the stars -- 2*1+1
+        for k in range(2*i+1):
+         print("*", end="")
+        #  Step 4: print a new line
+        print()
