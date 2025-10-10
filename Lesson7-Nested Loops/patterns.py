a4c672108a19c3086bd21b1141a84b90103220a7
DartM555 <williammejia1720@gmail.com>
2025-10-09 23:23:24 -0400

    Star Pyramid complete

--- Lesson7-Nested Loops/patterns.py
+++ Lesson7-Nested Loops/patterns.py
@@ -2,9 +2,18 @@
 # Row 0: 4 spaces, 1 star => 0 spaces, 9 stars
 
 rows = 5
-for i in range(rows):
-    for j in range(rows-i-1):
-        print(" " , end = "")
-        for k in range(2*i+1):
-            print("*")
-    
+spaces = rows - 1
+for row in range(1, rows + 1):
+   for space in range(spaces):
+       print(" ", end="")
+       
+       spaces -= 1
+       for stars in range(1, 2 * row):
+           print("*", end="")
+           print()
+        
+
+
+
+
+9
